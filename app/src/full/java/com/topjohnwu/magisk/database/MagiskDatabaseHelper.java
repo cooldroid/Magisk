bf500dd0b2836c82b2ced0f35598f56ad1f0ee16
CoolDroid <amit.bagaria@gmail.com>
2018-06-14 12:00:09 +0530

    Delete outdated policy
    (cherry picked from commit 22f878c7ef0d90355b1beeb0da0d62c7bbdb8c3c)
    (cherry picked from commit 52a9d3f7f0484dc61bea501c97b33bcefadaf5da)

--- app/src/full/java/com/topjohnwu/magisk/database/MagiskDatabaseHelper.java
+++ app/src/full/java/com/topjohnwu/magisk/database/MagiskDatabaseHelper.java
@@ -215,6 +215,7 @@ public class MagiskDatabaseHelper {
     }
 
     public void addPolicy(Policy policy) {
+        deletePolicy(policy.packageName);
         db.replace(POLICY_TABLE, null, policy.getContentValues());
     }
 
